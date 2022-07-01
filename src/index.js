52976be59ab88170e72a583c9e2b12791a03b8e8
Sorulai <57193937+Sorulai@users.noreply.github.com>
2022-07-01 22:18:08 +0300

    feat:add func reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let number = Math.abs(n).toString()
+    let res = ''
+    for (let i = (number.length - 1); i >= 0; i--) {
+        res += `${number[i]}`
+    }
+    return Number(res)
+
+
 }
